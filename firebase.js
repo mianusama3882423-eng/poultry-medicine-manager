aec84e4903082318a37c3b290d7d65499c4be661
mianusama3882423-eng <mianusama3882423@gmail.com>
2026-09-24 15:21:23 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,29 @@
+// firebase.js
+// Poultry Medicine Manager
+// Version 1.0.0
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_EYa-XcZptvyj3RpIkcNX6XPZXXgVdTE",
+  authDomain: "poultry-medicine-manager.firebaseapp.com",
+  projectId: "poultry-medicine-manager",
+  storageBucket: "poultry-medicine-manager.firebasestorage.app",
+  messagingSenderId: "701249686998",
+  appId: "1:701249686998:web:0e7fe4fb188947e2026877"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export { app };
